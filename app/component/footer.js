4615dd1a050d1ece3a2ac7a96cc61d1fa9e16875
0xdblok <dblok0x@gmail.com>
2024-04-21 11:13:25 +0200

    ee

--- app/component/footer.js
+++ app/component/footer.js
@@ -8,22 +8,12 @@ export default function Footer() {
   return (
     <div className="animate__animated animate__fadeInUp">
       <div className="flex gap-2 pt-10 justify-end pb-4 pr-6">
-        <a href="">
+        <a href="https://x.com/aabrahambase">
           {" "}
           <Image
             className="lg:w-[40px] lg:h-[40px]"
             src={xx}
             alt="fallout"
-            width={30}
-            height={30}
-          />
-        </a>
-        <a href="">
-          {" "}
-          <Image
-            className="lg:w-[40px] lg:h-[40px]"
-            src={tele}
-            alt="fallout "
             width={30}
             height={30}
           />
