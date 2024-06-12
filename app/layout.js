300b1a8f7218a7079626624b5da92e4346c06e04
0xdblok <dblok0x@gmail.com>
2024-06-13 00:29:31 +0200

    ee

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Abraham Lincoln",
-  description: "Abraham Lincoln",
+  title: "Nikula Tesla",
+  description: "Nikula Tesla",
 };
 
 export default function RootLayout({ children }) {
